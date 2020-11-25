c0f3eb55e4f97f724fd783d15886187decbd47a3
vladyslav1243 <galich.vinfort@gmail.com>
2020-11-25 15:45:36 +0200

    first commit

---
+++ app/script/main.js
@@ -0,0 +1,37 @@
+let myWidth = window.innerWidth,
+    myHeight = window.innerHeight;
+console.log(`width ${myWidth} \n height ${myHeight}`);
+
+window.onload = function () {
+
+    /*
+        fancybox settings
+     */
+
+    $.fancybox.defaults.loop = true;
+    $.fancybox.defaults.animationEffect = 'fade';
+
+    /*
+        form styler
+     */
+
+    $(function () {
+        $('select').styler({
+            selectSmartPositioning: false
+        });
+    });
+
+
+    // console.log(obj);
+
+
+    // /*
+    //     change href on mobile
+    //  */
+    //
+    // if(/iPhone|iPod|Android/i.test(navigator.userAgent)){
+    //     document.querySelector('a.grande').href = '#formgrande';
+    //     document.querySelector('a.lake').href = '#formlake';
+    //     document.querySelector('a.lou').href = '#formlou';
+    // }
+};
