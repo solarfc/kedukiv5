a3ed64902f803e14f4552e9eeff3aa6579203c2a
vladyslav1243 <galich.vinfort@gmail.com>
2020-11-26 17:48:29 +0200

    second commit

--- app/script/main.js
+++ app/script/main.js
@@ -21,6 +21,42 @@ window.onload = function () {
         });
     });
 
+    /*
+        review slider
+     */
+
+    $('.review__content-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 0,
+        speed: 300,
+        arrows: true,
+        prevArrow: $('.prev-arrow'),
+        nextArrow: $('.next-arrow')
+    });
+
+    // const headerImage = document.querySelector('.header__content-img figure img'),
+    //     toggleImage = () => {
+    //         headerImage.style.opacity = '0';
+    //         setTimeout(() => {
+    //             headerImage.src = 'img/header/shoe-second.png';
+    //         }, 1600);
+    //         setTimeout(() => {
+    //             headerImage.style.opacity = '1';
+    //         }, 1700);
+    //         setTimeout(() => {
+    //             headerImage.style.opacity = '0';
+    //         },3200);
+    //         setTimeout(() => {
+    //             headerImage.src = 'img/header/shoe-first.png';
+    //         }, 4800);
+    //         setTimeout(() => {
+    //             headerImage.style.opacity = '1';
+    //         }, 6300);
+    //     }
+    //     setInterval(toggleImage, 10000);
+    // setInterval(toggleImage, 2000);
+
 
     // console.log(obj);
 
